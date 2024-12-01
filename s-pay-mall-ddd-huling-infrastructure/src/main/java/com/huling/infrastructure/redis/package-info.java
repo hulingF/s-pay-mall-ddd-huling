38b5f3657fa136d1ab5c1dd6098b018afb76a4c2
斛零 <72245435+hulingF@users.noreply.github.com>
2024-12-01 11:16:15 +0800

    feat: init project

---
+++ s-pay-mall-ddd-huling-infrastructure/src/main/java/com/huling/infrastructure/redis/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * 提供redis链接配置
+ */
+package com.huling.infrastructure.redis;
---
+++ s-pay-mall-ddd-huling-types/src/main/java/com/huling/types/common/Constants.java
@@ -0,0 +1,7 @@
+package com.huling.types.common;
+
+public class Constants {
+
+    public final static String SPLIT = ",";
+
+}
